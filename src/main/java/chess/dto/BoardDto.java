0895dc99e2a705281587380a62c7a026ad04afc9
corinne <yoo77hyeon@gmail.com>
2022-04-16 12:48:05 +0900

    refactor: Converter 클래스 생성

--- src/main/java/chess/dto/BoardDto.java
+++ src/main/java/chess/dto/BoardDto.java
@@ -1,9 +1,7 @@
 package chess.dto;
 
 import chess.domain.member.Member;
-import chess.domain.pieces.Color;
 import chess.domain.pieces.Piece;
-import chess.domain.pieces.Symbol;
 import chess.domain.position.Column;
 import chess.domain.position.Row;
 
@@ -36,27 +34,9 @@ public class BoardDto {
     private static List<PieceDto> makeLine(Map<String, Piece> pieces, Integer row) {
         List<PieceDto> symbols = new ArrayList<>();
         for (Column column : Column.values()) {
-            symbols.add(PieceDto.of(findByKey(pieces, row, column), column.name() + row, background(row, column)));
+            symbols.add(PieceDto.of(ViewConverter.findImageName(pieces, row, column), column.name() + row, ViewConverter.findBackground(row, column)));
         }
         return symbols;
-    }
-
-    private static String background(Integer row, Column column) {
-        if ((column.value() + row) % 2 == 0) {
-            return "black";
-        }
-        return "white";
-    }
-
-    private static String findByKey(Map<String, Piece> pieces, Integer row, Column column) {
-        if (pieces.containsKey(row + column.name())) {
-            final Piece piece = pieces.get(row + column.name());
-            if(piece.isSameColor(Color.WHITE)) {
-                return "white-" + piece.symbol() + ".png";
-            }
-            return "black-" + piece.symbol() + ".png";
-        }
-        return Symbol.BLANK.value();
     }
 
     public List<List<PieceDto>> getBoardSymbols() {
